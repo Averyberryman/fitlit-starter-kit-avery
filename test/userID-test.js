1738d3a9dabb4e11f1da3a48b518051f3b43cf3b
Avery Berryman <averyberryman@gmail.com>
2023-07-10 15:45:38 -0600

    Add a test for getUserID function

--- test/userID-test.js
+++ test/userID-test.js
@@ -1,3 +1,10 @@
 import { expect } from 'chai';
+import sampleUsers from '../src/data/sample-users';
 import { getUserID } from '../src/userID';
 
+describe("getUserID", () => {
+    let users = sampleUsers;
+    it("should find a user object based on their unique ID", () => {
+        expect(getUserID(1).to.deep.equal(sampleUsers[0]))
+    }) 
+})
