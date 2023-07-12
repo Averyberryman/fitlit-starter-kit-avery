ef46e65882b308f254aad2746cbcf1076c84df60
Avery Berryman <averyberryman@gmail.com>
2023-07-12 14:23:25 -0600

    Add getDailyOunces function and passing test

--- test/hydrationFunc-test.js
+++ test/hydrationFunc-test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import sampleHydration from '../src/data/sample-hydration';
 import { averageDailyHydration } from '../src/hydrationFunctions';
+import { getDailyOunces } from '../src/hydrationFunctions';
+
 
 describe('average daily hydration', () => {
   const dataSet = sampleHydration.sampleHydrationData;
@@ -9,3 +11,23 @@ describe('average daily hydration', () => {
     expect(averageDailyHydration(userID, dataSet)).to.equal(33)
   })
 })
+
+
+
+describe('get user fluid ounces for a specific day', () => {
+  const dataSet = sampleHydration.sampleHydrationData;
+  const userID = 3;
+  const date = '2023/03/24';
+
+  it('should return user fluid ounces for a specific day', () => {
+    expect(getDailyOunces(userID, date, dataSet)).to.equal(95);
+  });
+
+  it('should return 0 if user fluid ounces are not available for the specific day', () => {
+    const nonExistingDate = '2023/03/27';
+    expect(getDailyOunces(userID, nonExistingDate, dataSet)).to.equal(0);
+  });
+});
+
+
+
