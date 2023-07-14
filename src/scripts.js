0fdd1aecb4f41045a1216e22c223f8f12fd27102
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-13 18:43:16 -0600

    Write dynamic API call function

--- src/scripts.js
+++ src/scripts.js
@@ -9,11 +9,6 @@ import './css/styles.css';
 import './images/turing-logo.png';
 
 // An example of how you tell webpack to use a JS file
-import userData from './data/users';
-console.log("User Data:", userData);
+import { APICall } from "./apiCalls"
 
-// Example of one way to import functions from the domUpdates file.  You will delete these examples.
-import { exampleFunction1, exampleFunction2 } from './domUpdates';
 
-exampleFunction1('Travis');
-exampleFunction2('Travis')
