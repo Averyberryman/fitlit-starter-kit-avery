43109afb3d8ff4949a4dc5e79b26a35d9850b237
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-13 18:46:31 -0600

    Write a Promise.all to assign data from promises

--- src/scripts.js
+++ src/scripts.js
@@ -21,3 +21,15 @@ const getSleepData = APICall("https://fitlit-api.herokuapp.com/api/v1/sleep");
 const getHydrationData = APICall("https://fitlit-api.herokuapp.com/api/v1/hydration");
 const getActivityData = APICall("https://fitlit-api.herokuapp.com/api/v1/activity");
 
+const getAllData = () => {
+  Promise.all([getUsersData, getSleepData, getHydrationData, getActivityData])
+  .then((data) => {
+      usersData = data[0];
+      sleepData = data[1];
+      hydrationData = data[2];
+      activityData = data[3];
+      console.log({usersData}, {sleepData}, {hydrationData}, {activityData})
+    }
+  )
+}
+
