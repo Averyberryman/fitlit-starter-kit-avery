fbda4ed357f9a0cb682fcc4a7e44084c414cd93f
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-15 16:59:29 -0600

    Get the API data to where we can use it

--- src/scripts.js
+++ src/scripts.js
@@ -1,42 +1,33 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png';
+import './images/turing-logo.png';
 
 // An example of how you tell webpack to use a JS file
-import { APICall } from "./apiCalls"
-import { getRandomUser } from './domUpdates';
+import { promises } from "./apiCalls"
+import {getRandomUser} from "./getRandomUser"
 
-let usersData;
-let sleepData;
-let hydrationData;
-let activityData;
+const mainData = {};
 
-const getUsersData = APICall("https://fitlit-api.herokuapp.com/api/v1/users");
-const getSleepData = APICall("https://fitlit-api.herokuapp.com/api/v1/sleep");
-const getHydrationData = APICall("https://fitlit-api.herokuapp.com/api/v1/hydration");
-const getActivityData = APICall("https://fitlit-api.herokuapp.com/api/v1/activity");
-
-const getAllData = () => {
-  Promise.all([getUsersData, getSleepData, getHydrationData, getActivityData])
-  .then((data) => {
-      usersData = data[0];
-      sleepData = data[1];
-      hydrationData = data[2];
-      activityData = data[3];
-      console.log({usersData}, {sleepData}, {hydrationData}, {activityData});
+    const startWebPage = () => {
+      console.log(getRandomUser(mainData.users))
+      
     }
-  );
-};
-
-window.addEventListener('load', () => {
-  getAllData()
-});
-window.addEventListener('load',  () => {
-  getRandomUser()
-});
+    window.addEventListener('load', () => {
+      Promise.all(promises)
+      .then(response => {
+        const [usersPromise, hyrdoPromise, sleepPromise, activityPromise] = response;
+        mainData.users = usersPromise;
+        mainData.hydration = hyrdoPromise;
+        mainData.sleep = sleepPromise;
+        mainData.activity = activityPromise;
+      })
+      .then(startWebPage
+        // dom func where you do all your shit
+        
+      )
+    });
