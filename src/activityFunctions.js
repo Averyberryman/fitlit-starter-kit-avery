b4d0fa665a8bb6a9f5c26e1b258fce3d573b9ff2
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-15 23:26:23 -0600

    Write milesWalkedOnDate function

---
+++ src/activityFunctions.js
@@ -0,0 +1,6 @@
+const milesWalkedOnDate = (userID, date, usersData, activityData) => {
+  const userStride = usersData.usersData.find((user) => user.id === userID).strideLength;
+  const userSteps = activityData.activityData.find((activity) => activity.userID === userID && activity.date === date).numSteps;
+  const totalFeetWalked = userStride * userSteps;
+  return Number((totalFeetWalked / 5280).toFixed(1));
+};
