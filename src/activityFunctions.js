cd1f3283b3ccffd9a70f7d8f9401343f82917ff0
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-15 23:30:25 -0600

    Write dailyActiveMinutes function

--- src/activityFunctions.js
+++ src/activityFunctions.js
@@ -4,3 +4,12 @@ const milesWalkedOnDate = (userID, date, usersData, activityData) => {
   const totalFeetWalked = userStride * userSteps;
   return Number((totalFeetWalked / 5280).toFixed(1));
 };
+
+const dailyActiveMinutes = (userID, date, activityData) => {
+return activityData.activityData.find((activity) => activity.userID === userID && activity.date === date).minutesActive
+}
+
+export { 
+  milesWalkedOnDate,
+  dailyActiveMinutes
+}
