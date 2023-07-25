13caa9c66fd63abd8fe5ddcb6d354ef862a93f74
Marisa Wyatt <risawyatt22@gmail.com>
2023-07-25 15:33:48 -0600

    Refactor to make more consistent

--- src/activityFunctions.js
+++ src/activityFunctions.js
@@ -1,28 +1,34 @@
 const milesWalkedOnDate = (userID, date, usersData, activityData) => {
   const userStride = usersData.find((user) => user.id === userID).strideLength;
-  const userSteps = activityData.find((activity) => activity.userID === userID && activity.date === date).numSteps;
+  const userSteps = activityData.find(
+    (activity) => activity.userID === userID && activity.date === date
+  ).numSteps;
   const totalFeetWalked = userStride * userSteps;
   return Number((totalFeetWalked / 5280).toFixed(1));
 };
 
 const dailyActiveMinutes = (userID, date, activityData) => {
-return activityData.find((activity) => activity.userID === userID && activity.date === date).minutesActive
-}
+  return activityData.find(
+    (activity) => activity.userID === userID && activity.date === date
+  ).minutesActive;
+};
 
 const getWeeklyStepGoals = (user, activityData) => {
-  const {id, dailyStepGoal} = user;
-  const filteredActivityData = activityData.filter((activity) => activity.userID === id);
+  const { id, dailyStepGoal } = user;
+  const filteredActivityData = activityData.filter(
+    (activity) => activity.userID === id
+  );
   const lastIndex = filteredActivityData.length - 1;
   const currentWeek = filteredActivityData.splice(lastIndex - 6, lastIndex);
   return currentWeek.map((day) => {
-    const {date, numSteps} = day;
+    const { date, numSteps } = day;
     const goalIsMet = numSteps >= dailyStepGoal;
-    return `${date}: ${goalIsMet ? 'pass' : 'fail'}`  
+    return `${date}: ${goalIsMet ? "pass" : "fail"}`;
   });
-}
+};
 
 export { 
-  milesWalkedOnDate,
-  dailyActiveMinutes,
-  getWeeklyStepGoals
+  milesWalkedOnDate, 
+  dailyActiveMinutes, 
+  getWeeklyStepGoals 
 }
