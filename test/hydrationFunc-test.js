5c28dc1749d47bc9f0718f165aa7de265654f18b
Avery Berryman <averyberryman@gmail.com>
2023-07-26 16:56:46 -0600

    Add tests for average daily hyration

--- test/hydrationFunc-test.js
+++ test/hydrationFunc-test.js
@@ -6,6 +6,10 @@ import { getDailyOunces, getWeeklyOunces } from '../src/hydrationFunctions';
 describe('average daily hydration', () => {
   const dataSet = sampleHydration.sampleHydrationData;
   const userID = 1
+  it('should be a function', () => {
+    expect(typeof averageDailyHydration).to.equal('function')
+  })
+  
   it('should calculate average daily hydration', () => {
     expect(averageDailyHydration(userID, dataSet)).to.equal(50)
   })
