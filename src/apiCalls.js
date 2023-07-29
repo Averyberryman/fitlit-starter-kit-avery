7468defd29406f3d5d27d4b1a6158cd82ca0ff41
Avery Berryman <averyberryman@gmail.com>
2023-07-29 16:47:33 -0600

    Achieved post functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+import { currentUser } from "./scripts";
+
 const usersPromise = fetch("https://fitlit-api.herokuapp.com/api/v1/users")
   .then((response) => response.json())
   .then((data) => data.users);
@@ -6,24 +8,28 @@ const hydroPromise = fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
   .then((response) => response.json())
   .then((data) => data.hydrationData);
 
-const hydroInput = fetch("http://localhost:3001/api/v1/hydration", {
-  method: 'POST',
-  body: JSON.stringify({ 
-    userID,
-    date,
-    numOunces,
-  }),
-  headers: {'Content-Type': 'application/json'}
-})
-.then((response) => response.json())
-.then((data) => data.hydrationData);
+export const hydroInput = (userID, date, numOunces) => {
+  fetch("http://localhost:3001/api/v1/hydration", {
+    method: "POST",
+    body: JSON.stringify({
+      userID,
+      date,
+      numOunces,
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
 
 const sleepPromise = fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
   .then((response) => response.json())
   .then((data) => data.sleepData);
 
 const activityPromise = fetch(
-  "https://fitlit-api.herokuapp.com/api/v1/activity")
+  "https://fitlit-api.herokuapp.com/api/v1/activity"
+)
   .then((response) => response.json())
   .then((data) => data.activityData);
 
